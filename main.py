c47c75e4caeb5102391369e482a01cfbdcd9208e
Dmitriy Ivanov <kurwatea@gmail.com>
2021-12-14 15:02:17 +0300

    code decoration

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
 import re
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-# pprint(contacts_list)
 
-# TODO 1: выполните пункты 1-3 ДЗ
+
 formatted_contacts_list = list()
+
+# Паттерны для регулярных выражений
 patterns = {
     'name': r'[а-яА-ЯёЁ]+',
     'split': r'[,\s]',
@@ -19,21 +19,33 @@ patterns = {
 
 
 for contact in contacts_list[1:]:
-    contact_full_name = ' '.join(contact[0:3])
-    formatted_name = re.split(pattern=patterns['split'], string=contact_full_name)
+
+    # Добавление имени
+    full_name = ' '.join(contact[0:3])
+    formatted_name = re.split(pattern=patterns['split'], string=full_name)
     formatted_contact = formatted_name[0:3]
+
+    # Добавление организации
     organization = contact[3]
     formatted_contact.append(organization)
+
+    # Добавление должности
     position = contact[4]
     formatted_contact.append(position)
+
+    # Добавление телефона
     phone = contact[5]
     formatted_phone = re.sub(pattern=patterns['phone'], repl=patterns['phone_replace'], string=phone)
     if formatted_phone:
         if formatted_phone[-1] == ' ':
             formatted_phone = formatted_phone[0:-1]
     formatted_contact.append(formatted_phone)
+
+    # Добавление почты
     email = contact[6]
     formatted_contact.append(email)
+
+    # Проверка совпадений записи
     found = False
     for cont in formatted_contacts_list:
         if formatted_name[0:2] == cont[0:2]:
@@ -44,10 +56,9 @@ for contact in contacts_list[1:]:
     if not found:
         formatted_contacts_list.append(formatted_contact)
 
+# Добавление строки с заголовками
 formatted_contacts_list.insert(0, contacts_list[0])
-pprint(formatted_contacts_list)
 
-# TODO 2: сохраните получившиеся данные в другой файл
 with open("phonebook.csv", "w", encoding='utf-8', newline='') as f:
     datawriter = csv.writer(f, delimiter=',')
     # Вместо contacts_list подставьте свой список
